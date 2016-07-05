ee7259df6cf2ead90551f18e7642aed01cd67f7d
Luis Fernandes <sednanref2004@gmail.com>
2016-07-04 22:11:05 -0400

    Exercise 2.2.1 Ionic Lists, advanced features: implemented an Add to Favorites Button into the menu view.

--- Ionic/conFusion/www/js/controllers.js
+++ Ionic/conFusion/www/js/controllers.js
@@ -71,7 +71,7 @@ angular.module('conFusion.controllers', [])
   };    
 })
 
-.controller('MenuController', ['$scope', 'menuFactory', 'baseURL', function($scope, menuFactory, baseURL) {
+.controller('MenuController', ['$scope', 'menuFactory', 'favoriteFactory', 'baseURL', '$ionicListDelegate', function ($scope, menuFactory, favoriteFactory, baseURL, $ionicListDelegate) {
 
             $scope.baseURL = baseURL;
             $scope.tab = 1;
@@ -114,6 +114,12 @@ angular.module('conFusion.controllers', [])
             $scope.toggleDetails = function() {
                 $scope.showDetails = !$scope.showDetails;
             };
+
+            $scope.addFavorite = function (index) {
+                console.log("index is " + index);
+                favoriteFactory.addToFavorites(index);
+                $ionicListDelegate.closeOptionButtons();
+            }
         }])
 
         .controller('ContactController', ['$scope', function($scope) {
